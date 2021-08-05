7c5e8938af90883c5ff03ec6d172a0bda2b87623
ttddd <50779378+tttdd@users.noreply.github.com>
2021-08-05 18:01:56 +0300

    добавлен роутинг для пагинации

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,34 @@ import React from 'react';
 import './App.css';
 import Title from './UI/Main/Title';
 import Main from './UI/Main/Main';
-import {BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import NewsItem from './UI/News/NewsItem';
 
-function App() {
+const App = () => {
   return (
-      <BrowserRouter>
-          <Route exact path="/" render={() =>
-              <div className="App">
-                  <Title title={()=>'HackerNews'}></Title>
-                  <Main/>
-              </div>
-          }/>
-          <Route path="/news" render={()=><NewsItem/>}/>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Route
+        exact
+        path="/"
+        render={() => (
+          <div className="App">
+            <Title title={() => 'HackerNews'}></Title>
+            <Main />
+          </div>
+        )}
+      />
+      <Route
+        path="/page"
+        render={() => (
+          <div className="App">
+            <Title title={() => 'HackerNews'}></Title>
+            <Main />
+          </div>
+        )}
+      />
+      <Route path="/news" render={() => <NewsItem />} />
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
