6573d3063bd759b0b18d598ff0a5658a9b58b328
ttddd <50779378+tttdd@users.noreply.github.com>
2021-08-15 22:22:39 +0300

    добавлен метод setPage

--- src/BLL/store/News.ts
+++ src/BLL/store/News.ts
@@ -1,16 +1,16 @@
 import { makeAutoObservable } from 'mobx';
-import { NewsI } from '../../UI/Interface';
+import { NewsI } from '../../UI/Type';
 import axios from 'axios';
 
 class News {
   newsList: NewsI[] = [];
   pagination = 1;
-
   constructor() {
     makeAutoObservable(this);
   }
-
   addNews = async () => {
+    // eslint-disable-next-line no-console
+    console.log('Я тут');
     this.newsList = [];
     for (let i = 1; i < 5; i++) {
       await axios.get<NewsI[]>(`https://api.hnpwa.com/v0/news/${i}.json`).then((response) => {
@@ -20,8 +20,9 @@ class News {
       });
     }
   };
-  changePagination(newPag) {
-    this.pagination = newPag;
-  }
+  setPage = (): void => {
+    if (document.location.href.split('/').includes('page'))
+      this.pagination = Number(document.location.href.split('/').slice(-1));
+  };
 }
 export default new News();
