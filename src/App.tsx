fa380c9494643c8af4a6a891324a439e19e35173
ttddd <50779378+tttdd@users.noreply.github.com>
2021-08-15 22:17:30 +0300

    изменен принцип пагинации, добавлен роутинг

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,29 @@ import React from 'react';
 import './App.css';
 import Title from './UI/Main/Title';
 import Main from './UI/Main/Main';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import NewsItem from './UI/News/NewsItem';
+import News from './BLL/store/News';
+import { observer } from 'mobx-react-lite';
 
-const App = () => {
+const App = observer(() => {
+  const get = () => {
+    let mas: any[] = [];
+    for (let i = 1; i < 5; i++) {
+      mas.push(
+        <Route
+          key={i}
+          path={`/page/${i}`}
+          render={() => (
+            <div className="App">
+              <Main />
+            </div>
+          )}
+        />,
+      );
+    }
+    return mas;
+  };
   return (
     <BrowserRouter>
       <Route
@@ -23,13 +42,13 @@ const App = () => {
         render={() => (
           <div className="App">
             <Title title={() => 'HackerNews'}></Title>
-            <Main />
           </div>
         )}
       />
+      <Switch>{get()}</Switch>
       <Route path="/news" render={() => <NewsItem />} />
     </BrowserRouter>
   );
-};
+});
 
 export default App;
